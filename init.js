06e61f593c84d2ad6675faed4a4ad9017af78e41
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-25 20:20:16 +0100

    init

--- init.js
+++ init.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
+import inquirer from 'inquirer';
+import fsExtra from 'fs-extra';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { promises as fs } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const inquirer = await import('inquirer');
-const fsExtra = await import('fs-extra');
+const main = async () => {
+	const answers = await inquirer.prompt([{
+		type: 'input',
+		name: 'projectName',
+		message: 'Project name:',
+		validate: input => input ? true : 'Project name cannot be empty.'
+	}]);
 
-inquirer.prompt([{
-	type: 'input',
-	name: 'projectName',
-	message: 'Project name:',
-	validate: input => input ? true : 'Project name cannot be empty.'
-}]).then(async answers => {
 	const projectName = answers.projectName;
 	const projectPath = path.join(process.cwd(), projectName);
 	const templatePath = path.join(__dirname, 'template');
@@ -30,13 +30,18 @@ inquirer.prompt([{
 
 	// Read the package.json from the template
 	const packageJsonPath = path.join(projectPath, 'package.json');
-	const packageJson = JSON.parse(await fs.readFile(packageJsonPath, 'utf8'));
+	const packageJson = JSON.parse(await fsExtra.readFile(packageJsonPath, 'utf8'));
 
 	// Modify the project name
 	packageJson.name = projectName;
 
 	// Write the modifications
-	await fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
+	await fsExtra.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
 
 	console.log(`Project ${projectName} has been successfully created. Please follow the README instructions to get started.`);
+};
+
+main().catch(e => {
+	console.error(e);
+	process.exit(1);
 });
